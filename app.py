fec1c0b9aeea65bfa07a301a428af064b174cae8
“yumiazusa” <yumiazusa@hotmail.com>
2026-01-27 00:48:46 +0800

    5600

--- app.py
+++ app.py
@@ -255,4 +255,4 @@ def export_excel():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5600)
